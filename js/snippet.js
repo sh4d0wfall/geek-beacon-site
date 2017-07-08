928c25d9dbe7e92adccc012738374f7794ca17fa
Samir Faci <samir@esamir.com>
2017-07-07 21:07:22 -0700

    Fixing offset

---
+++ js/snippet.js
@@ -0,0 +1,8 @@
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+    $("#menu").load("common/buttons.html");
+    $("#header").load("common/header.html");
+    $("#footer").load("common/footer.html");
+});
+</script>
